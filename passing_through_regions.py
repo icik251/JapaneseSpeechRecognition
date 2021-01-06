e8501164450a059837f891d4d4fae217be044935
Ritten <ritten98@hotmail.com>
2021-01-06 23:09:03 +0100

    Feature extraction seems to be working corretly. Still needs comments

---
+++ passing_through_regions.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+
+class Feature_selection:
+
+    def __init__(self, filepath, U=4, V=5, K=3):
+        self.df = pd.read_pickle(filepath)
+        self.U = U
+        self.V = V
+        self.n = len(self.df[0][0])
+        self.K = K
+        self.bins = self.init_bins()
+
+    def init_bins(self, U=4, V=5):
+        max_length, y_min, y_max = self.find_limits()
+        x_theta = np.arange(0, max_length+1, (max_length/(self.U-1))).astype(int)
+        print(f"x_theta values:{x_theta}")
+        y_theta = np.arange(y_min, y_max+0.01, ((y_max-y_min)/(self.V-1)))
+        print(f"y_theta values:{y_theta}")
+        idx = 0
+        bins = np.empty(self.number_of_rectangles(), dtype=object)
+        for x1 in range(0, (self.U-1)):
+            for x2 in range((x1+1), self.U):
+                for y1 in range(0, (self.V-1)):
+                    for y2 in range((y1+1), self.V):
+                        bins[idx] = Bin(idx, x_theta[x1], x_theta[x2], y_theta[y1], y_theta[y2])
+                        idx += 1
+        return bins
+
+    def get_features(self):
+        feature_matrix = {}
+        for sample_idx in range(len(self.df)):
+            feature_matrix[sample_idx] = self.determine_feature_vector(self.df[sample_idx])
+        return feature_matrix
+
+    def determine_feature_vector(self, signal):
+        feature_vector = np.empty(self.length_feature_vector())
+        idx = 0
+        for bin in self.bins:
+            for k in range(1, self.K+1):
+                passes = bin.count_passes_for_each_dimension(signal, self.n)
+                print(passes)
+                for entry in passes:
+                    feature_vector[idx] = 1 if entry >= k else 0
+                    idx += 1
+                    feature_vector[idx] = 1 if entry < k else 0
+        return feature_vector
+
+
+    def length_feature_vector(self):
+        '''
+        Returns the length of the feature vector by multiplying the total amount of possible rectangles
+         with:
+        - how often the signal should pass through the bin (k).
+        - the dimensions of the signal (n).
+        This is multiplied by 2 to account for the inverted bins as well
+        :return: the length of the feature vector
+        :rtype: int
+        '''
+        return 2*self.n*self.K*self.number_of_rectangles()
+
+    def number_of_rectangles(self):
+        '''
+        Formula used to calculate the possible rectangles within a grid. Based on formula found in:
+        https://math.stackexchange.com/questions/1656686/how-many-rectangles-can-be-observed-in-the-grid
+        :return: number of possible rectangles
+        :rtype: int
+        '''
+        U = self.U
+        V = self.V
+        return int((((U-1)*U)/2)*(((V-1)*V)/2))
+
+    def find_limits(self):
+        y_min = float('inf')
+        y_max = float('-inf')
+
+        max_length = 0
+
+        for sample in self.df:
+            if len(sample) > max_length:
+                max_length = len(sample)
+            for entry in sample:
+                for value in entry:
+                    if value > y_max:
+                        y_max = value
+                    if value < y_min:
+                        y_min = value
+        return max_length, y_min, y_max
+
+class Bin:
+    def __init__(self, idx, x1, x2, y1, y2):
+        '''
+        The bins are used as a separate class to make it easier to determine how often a timeseries segment passes
+        though the bin.
+        :param idx: Identifier of the bin
+        :type: int
+        :param x1: The x-coordinate of the left-most boundary
+        :type x1: int
+        :param x2: The x-coordinate of the right-most boundary
+        :type x2: int
+        :param y1: The y-coordinate of the bottom boundary
+        :type y1: float
+        :param y2: The y-coordinate of the top boundary
+        :type y2: float
+        '''
+        self.idx = idx
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+    def count_passes_for_each_dimension(self, signal, n=12):
+        relevant_signal = signal[self.x1:self.x2+1]
+        return self.count_passes_recursive(relevant_signal, n)
+
+    def count_passes_recursive(self, remaining_signal, n=12, inside=np.full((12), False)):
+        if len(remaining_signal) == 0:  # the end of the relevant signal has been reached
+            return np.zeros(n)
+
+        passes = np.zeros(n)
+        next_iteration_inside = np.full((n), False)
+        for dim in range(n):
+            if inside[dim]:
+                if self.is_within_box(remaining_signal[0][dim]):
+                    next_iteration_inside[dim] = True
+            else:
+                if self.is_within_box(remaining_signal[0][dim]):
+                    next_iteration_inside[dim] = True
+                    passes[dim] += 1
+        passes_total = np.add(passes, self.count_passes_recursive(remaining_signal[1:], n, next_iteration_inside))
+        return passes_total
+        #
+        # if inside:
+        #     if self.is_within_box(remaining_signal[0]):  # the signal remains inside the bin
+        #         return self.count_passes_recursive(remaining_signal[1:], n, True)
+        #     else:  # the signal has exited the bin
+        #         return self.count_passes_recursive(remaining_signal[1:], n, False)
+        # else:
+        #     if self.is_within_box(remaining_signal[0]):  # the signal has reentered the bin
+        #         return 1 + self.count_passes_recursive(remaining_signal[1:], n, True)
+        #     else:  # the signal remains outside of the bin
+        #         return self.count_passes_recursive(remaining_signal[1:], n, False)
+        # return
+
+    def is_within_box(self, y):
+        return self.y1 <= y <= self.y2
+
+    def __str__(self):
+        return f"idx: {self.idx} - x:({self.x1},{self.x2}) - y:{self.y1},{self.y2})"
+
+
+features = Feature_selection("Data"+os.sep+"test_inputs.pkl")
+max_length, y_min, y_max = features.find_limits()
+print(f"max_length: {max_length}, y_min: {y_min}, y_max: {y_max}")
+result = features.get_features()
