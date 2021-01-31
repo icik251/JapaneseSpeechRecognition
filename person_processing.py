944850f28121a6162955210728fb6d19739ebf22
icik251 <hristostanulov@gmail.com>
2021-01-31 03:33:53 +0100

    neural net prototype

--- person_processing.py
+++ person_processing.py
@@ -18,48 +18,35 @@ class PersonProcessing:
         result = array[np.round(np.linspace(0, len(array)-1, samples)).astype(int)]
         return result
     
-    def get_segmentation(self, chunks=2):
+    def get_segmentation(self, k=2):
         """[Segments the mean value of each chunk from each feature vector (each cepstrum coefficient vector (12-vectors per person)). 
         Then concatanates the result vectors. This way the different length of the time series doesn't matter.]
 
         Args:
-            chunks (int, optional): [How many chunks do you want to separate the feature vector]. Defaults to 2.
+            k (int, optional): [How many k samples do you want to separate the feature vector]. Defaults to 2.
 
         Returns:
-            [ndarray]: [Numpy array resulted from the concatanation of all means from the chunks.]
+            [ndarray]: [Numpy array resulted from the concatanation of all means from the k samples.]
         """
         list_of_results = []
         for n_array in self.person_array.T:
-            list_of_results.append(self._segmentation(n_array, chunks))
+            list_of_results.append(self._segmentation(n_array, k))
             
         concat_results = np.concatenate(list_of_results)
         return concat_results
-    
-    def get_sampling(self, samples=2):
+
+    def get_sampling(self, k=2):
         """[Samples data points from each feature vector (each cepstrum coefficient vector (12-vectors per person)). Then concatanates the samples]
 
         Args:
-            samples (int, optional): [Number of samples that we want to obtain for each feature vector. Defaults to 2.
+            k (int, optional): [Number of k samples that we want to obtain for each feature vector. Defaults to 2.
 
         Returns:
-            [ndarray]: [Numpy array resulted from the concatanation of all samples obtained from the cepstrum coefficients.]
+            [ndarray]: [Numpy array resulted from the concatanation of all k samples obtained from the cepstrum coefficients.]
         """
         list_of_results = []
         for n_array in self.person_array.T:
-            list_of_results.append(self._sampling(n_array, samples))
+            list_of_results.append(self._sampling(n_array, k))
         
         concat_results = np.concatenate(list_of_results)
         return concat_results
-    
-    
-with open('Data\\train_inputs.pkl', 'rb') as handle:
-    list_of_trains = pickle.load(handle)
-    
-test_first_person = list_of_trains[0]
-
-person_processing_obj = PersonProcessing(test_first_person)
-results_sampling = person_processing_obj.get_sampling()
-results_segmentation = person_processing_obj.get_segmentation()
-
-print(results_sampling)
-print(results_segmentation)
